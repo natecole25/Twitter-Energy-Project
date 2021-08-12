ca6c23964c991d87c029604ef72d551f3ffbd3dc
Nathaniel Cole <nwc13@duke.edu>
2021-08-12 11:17:55 -0400

    fix migration file tweets table name

--- db/migrate/20210728174929_create_tweets_table.rb
+++ db/migrate/20210728174929_create_tweets_table.rb
@@ -1,6 +1,6 @@
 class CreateTweetsTable < ActiveRecord::Migration[6.1]
   def change
-    create_table :tweets_tables do |t|
+    create_table :tweets do |t|
     t.integer "tweet_id"
     t.text "tweet_text"
     t.integer "retweet_count"
