1e06cd623abcea809cfdb4dafe70d3f5fab8e2e6
Nathaniel Cole <nwc13@duke.edu>
2021-08-16 18:32:46 -0400

    Fixing the foreign key relationship dependency that prevented rule delete

--- app/models/tweet_rule.rb
+++ app/models/tweet_rule.rb
@@ -1,6 +1,7 @@
 class TweetRule < ApplicationRecord
 
-    has_many :tweets
+    #Doing so will result in the removal of all tweets that were picked up for the rule being deleted. 
+    has_many :tweets, dependent: :destroy 
 
 
 end
